4ead1a99e05baad8e1a5b78ebceaff511c589bfa
Tamires <tamires.lowande@gmail.com>
2019-10-29 16:36:40 -0400

    helper function that receives a state and returns an array of appointments

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+
+export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter(d => d.name === day);
+  const dayAppointments = filteredDay[0]
+  if(dayAppointments === undefined || dayAppointments.length === 0){
+    return []
+  } else {
+    const detailedAppointments= dayAppointments.appointments.map(id =>  state.appointments[id])
+    return detailedAppointments    
+  }
+}
