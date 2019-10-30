4dc4268bb26d6cbe62c8f53a90d8391dbef16e35
Tamires <tamires.lowande@gmail.com>
2019-10-29 20:11:05 -0400

    adds the interviewer name to the page by using a helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./appointment/index";
-import getAppointmentsForDay from "../helpers/selectors";
+import { getAppointmentsForDay,getInterview } from "../helpers/selectors";
 import axios from "axios";
 
 // const days = [
@@ -65,33 +65,49 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {}
   })
-  
-  const appointments = getAppointmentsForDay(state, state.day)
-  const setDay = day => setState(prev => ({ ...prev, day }));
-  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.all([
       axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments")
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
     ])
       .then((all) => {
         setState(prev => ({
           ...prev,
           days: all[0].data,
-          appointments: all[1].data
+          appointments: all[1].data,
+          interviewers: all[2].data
         }))
       })
-
       // .then(resp => setDays(resp.data)) //setState({...state, days: resp.data})) 
       // .then(setTimeout(() => console.log(state), 4000))
       .catch((error) => {
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        console.log(error.response.data);
+        // console.log(error.response.status);
+        // console.log(error.response.headers);
+        // console.log(error.response.data);
       });
   }, [])
+
+  const appointments = getAppointmentsForDay(state, state.day)
+  console.log(appointments)
+  // const schedule = appointments.map((appointment) => {
+  //   const interview = getInterview(state, appointment.interview);
+
+  //   return (
+  //     <Appointment
+  //       key={appointment.id}
+  //       id={appointment.id}
+  //       time={appointment.time}
+  //       interview={interview}
+  //     />
+  //   );
+  // });
+
+  const setDay = day => setState(prev => ({ ...prev, day }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   return (
     <main className="layout">
@@ -117,15 +133,17 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointments.map((e) => {
+
+          const interview = getInterview(state, e.interview); 
           return (
             <Appointment
               key={e.id}
-              // {id={e.id}
-              // time={e.time}
-              // interview={e.interview}}
-              {...e}
+              id = { e.id }
+              time={e.time}
+              interview={interview}
+              // {...e}
             />
-          )
+        )
         })}
         <Appointment
           key="last"
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,24 @@
 
-export default function getAppointmentsForDay(state, day) {
+function getAppointmentsForDay(state, day) {
   const filteredDay = state.days.filter(d => d.name === day);
   const dayAppointments = filteredDay[0]
-  if(dayAppointments === undefined || dayAppointments.length === 0){
+  if (dayAppointments === undefined || dayAppointments.length === 0) {
     return []
   } else {
-    const detailedAppointments= dayAppointments.appointments.map(id =>  state.appointments[id])
-    return detailedAppointments    
+    const detailedAppointments = dayAppointments.appointments.map(id => state.appointments[id])
+    return detailedAppointments
   }
 }
+
+function getInterview(state, interview) {
+  if(!interview) return null;
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer]
+
+  }
+}
+  export {
+    getAppointmentsForDay,
+    getInterview
+  }
