dc01bc128c7cfa9a5825b640fcceea88c6388c8c
Tamires <tamires.lowande@gmail.com>
2019-11-05 11:55:38 -0500

    adds integration test to verify the book interview process'

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,62 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+  prettyDOM,
+  getByText,
+  getByPlaceholderText,
+  getByAltText,
+  getAllByTestId
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
+//_____________________________
+
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
-});
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday"))
+      .then(res => {
+        fireEvent.click(getByText("Tuesday"));
+        expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      })
+    //option to use await-async (add "async" to the "it" function)
+    // await waitForElement(() => getByText("Monday"))
+    //fireEvent.click(getByText("Tuesday"));
+    //expect(getByText("Leopold Silvers")).toBeInTheDocument();
+
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />)
+
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+    // console.log(prettyDOM(container))
+
+    const appointments = getAllByTestId(container, "appointment");
+    // console.log(prettyDOM(appointments));
+    const appointment = appointments[0]
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // console.log(prettyDOM(appointment))
+  })
+
+
+}) 
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,7 +10,6 @@ import axios from "axios";
 
 export default function useApplicationData() {
 
-
   const SET_DAY = "SET_DAY";
   const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
   const SET_INTERVIEW = "SET_INTERVIEW";
@@ -50,28 +49,31 @@ export default function useApplicationData() {
   })
   //--------------------------------
   useEffect(() => {
-  axios.all([
-    axios.get("http://localhost:8001/api/days"),
-    axios.get("http://localhost:8001/api/appointments"),
-    axios.get("http://localhost:8001/api/interviewers")
-  ])
-    .then((all) => {
-      dispatch({
-        type: "SET_APPLICATION_DATA",
-        value: {days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data}
 
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ])
+      .then((all) => {
+        dispatch({
+          type: "SET_APPLICATION_DATA",
+          value: {
+            days: all[0].data,
+            appointments: all[1].data,
+            interviewers: all[2].data
+          }
+
+        })
       })
-    })
-    
-    // .then(resp => setDays(resp.data)) //setState({...state, days: resp.data})) 
-    // .then(setTimeout(() => console.log(state), 4000))
-    .catch((error) => {
-      console.error(error.response.status);
-      console.error(error.response.headers);
-      console.error(error.response.data);
-    });
+
+      // .then(resp => setDays(resp.data)) //setState({...state, days: resp.data})) 
+      // .then(setTimeout(() => console.log(state), 4000))
+      .catch((error) => {
+        console.error(error.response.status);
+        console.error(error.response.headers);
+        console.error(error.response.data);
+      });
   }, [])
   // console.log('state in useApp is', state)
 
@@ -128,10 +130,18 @@ export default function useApplicationData() {
   }
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-    .then(resp => {
-      dispatch({ type:"SET_SPOTS", value: resp.data })
-    })
+
+    async function fetchData() {
+      let resp = await axios.get("http://localhost:8001/api/days");
+      dispatch({ type: "SET_SPOTS", value: resp.data })
+    }
+
+    fetchData();
+    // axios.get("http://localhost:8001/api/days")
+    //   .then(resp => {
+    //     console.log(resp.data)
+    //     dispatch({ type: "SET_SPOTS", value: resp.data })
+    //   })
   }, [state.appointments])
 
   return {
