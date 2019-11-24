f66377d67f6c48d0d7bc4b73fd87394af853e989
Tamires <tamires.lowande@gmail.com>
2019-11-24 14:28:14 -0500

    adds validation for interviewer not to be blank

--- src/components/appointment/Form.js
+++ src/components/appointment/Form.js
@@ -23,6 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("Interviewer cannot be blank");
+      return;
+    }
     setError("")
     props.onSave(name, interviewer, props.isSave);
   }
